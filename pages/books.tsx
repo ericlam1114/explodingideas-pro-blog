ed72ae6a583dbaa6dcaf3534b9c26726b16c6864
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-05-19 08:11:58 -0700

    books page design

--- pages/books.tsx
+++ pages/books.tsx
@@ -8,6 +8,12 @@ import Link from 'next/link'
 export default function Home() {
   const [filter, setFilter] = useState("");
 
+  // Dummy book data for illustration
+  const books = [
+    {title: 'How to Validate Your Idea', author: 'Explodingideas', img: '/images/book1.jpg', description: 'A short description about this book', price: '$20.00'},
+    //... other books
+  ]
+
   return (
     <div>
       <Head>
@@ -21,26 +27,21 @@ export default function Home() {
         <h2 className="flex justify-center  gap-8 uppercase text-xl pt-8">
           Startup Library
         </h2>
-        {/* Add a search bar */}
-        {/* <input
-          type="text"
-          value={filter}
-          onChange={(e) => setFilter(e.target.value)}
-          placeholder="Search articles..."
-          className="w-2/3 mx-auto px-4 py-2 my-4 text-lg text-gray-700 bg-white border border-gray-300 rounded-md shadow focus:outline-none"
-          style={{ fontSize: "1.2rem" }}
-        /> */}
 
         <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 py-6">
-          <div className="border border-gray-200 p-4 rounded-md shadow-lg"> <Link href="/books/howtovalidateyouridea">How to Validate Your Idea</Link>
-          
-          </div>
-          <div className="border border-gray-200 p-4 rounded-md shadow-lg">
-          Contacts: Traditional Venture Capital
-          </div>
-          <div className="border border-gray-200 p-4 rounded-md shadow-lg">
-            Publications: Crypto Journalists
-          </div>
+          {books.map((book) => (
+            <Link key={book.title} href="/books/howtovalidateyouridea">
+              <div className="border border-gray-200 p-4 rounded-md shadow-lg cursor-pointer"> 
+                  <img src={book.img} alt={book.title} className="w-full h-64 object-cover rounded" />
+                  <h3 className="mt-2 text-lg font-semibold">{book.title}</h3>
+                  <p className="text-gray-500">By {book.author}</p>
+                  <p className="mt-2 text-sm">{book.description}</p>
+                  <div className="mt-3">
+                    <span className="text-lg font-semibold">{book.price}</span>
+                  </div>
+              </div>
+            </Link>
+          ))}
         </div>
         <Footer />
       </main>
